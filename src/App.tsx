45be32308fb1f6decb6876233f386231bc8d11f9
krivovDima <krivov.dmitry89@gmail.com>
2021-11-14 15:02:07 +0400

    add jsx in registration page

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,17 @@ import TestPage from "./components/TestPage/TestPage";
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<TestPage/>}/>
-        <Route path="login" element={<Login/>}/>
-        <Route path="passwordRecovery" element={<PasswordRecovery/>}/>
-        <Route path="profile" element={<Profile/>}/>
-        <Route path="registration" element={<Registration/>}/>
-        <Route path="inputNewPassword" element={<InputNewPassword/>}/>
-      </Routes>
+      <div className='AppInner'>
+        <Routes>
+          <Route path="/" element={<TestPage/>}/>
+          <Route path="login" element={<Login/>}/>
+          <Route path="passwordRecovery" element={<PasswordRecovery/>}/>
+          <Route path="profile" element={<Profile/>}/>
+          <Route path="registration" element={<Registration/>}/>
+          <Route path="inputNewPassword" element={<InputNewPassword/>}/>
+        </Routes>
+      </div>
+
     </div>
   );
 }
--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import styles from './Registration.module.css';
 
 function Registration() {
-  return (
-    <div>
-      Registration
-    </div>
-  );
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.box}>
+                <h2 className={styles.title}>It-incubator</h2>
+                <h3 className={styles.subTitle}>Sign Up</h3>
+                <div className={styles.formWrapper}>
+                    <input className={styles.input} type="text"/>
+                    <input className={styles.input} type="password"/>
+                    <input className={styles.input} type="password"/>
+                    <div className={styles.btns}>
+                        <button className={styles.btn}>Cancel</button>
+                        <button>Register</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Registration;
