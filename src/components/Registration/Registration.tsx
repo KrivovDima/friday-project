fb527ec08735536bf28c3fda601906ec5d3e1210
krivovDima <krivov.dmitry89@gmail.com>
2021-11-14 18:27:34 +0400

    add react hook form for registration form

--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -1,21 +1,62 @@
 import React from 'react';
 import styles from './Registration.module.css';
+import {SubmitHandler, useForm} from "react-hook-form";
+
+type InputsType = {
+    email: string
+    password: string
+    confirmPassword: string
+}
 
 function Registration() {
+    const {register, handleSubmit, watch, formState: {errors}} = useForm<InputsType>({mode: "onBlur"});
+    const passwordValue = watch("password");
+    const onSubmit: SubmitHandler<InputsType> = data => console.log(data);
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.box}>
                 <h2 className={styles.title}>It-incubator</h2>
                 <h3 className={styles.subTitle}>Sign Up</h3>
-                <div className={styles.formWrapper}>
-                    <input className={styles.input} type="text"/>
-                    <input className={styles.input} type="password"/>
-                    <input className={styles.input} type="password"/>
+                <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+                    <div className={styles.inputWrapper}>
+                        <input placeholder='Email'
+                               className={styles.input}
+                               type="text"
+                               {...register("email", {
+                                   required: {value: true, message: 'Email is required'},
+                                   pattern: {
+                                       value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                       message: 'Invalid email'
+                                   },
+                               })}/>
+                        {errors.email && <div className={styles.error}>{errors.email.message}</div>}
+                    </div>
+                    <div className={styles.inputWrapper}>
+                        <input placeholder='Password'
+                               className={styles.input}
+                               type="password"
+                               {...register("password", {
+                                   required: {value: true, message: 'Password is required'},
+                                   minLength: {value: 4, message: 'Must be at least 4 characters'},
+                               })}/>
+                        {errors.password && <div className={styles.error}>{errors.password.message}</div>}
+                    </div>
+                    <div className={styles.inputWrapper}>
+                        <input placeholder='Confirm password'
+                               className={styles.input}
+                               type="password"
+                               {...register("confirmPassword", {
+                                   required: {value: true, message: 'Confirm password is required'},
+                                   validate: value => value === passwordValue || 'Password mismatch'
+                               })}/>
+                        {errors.confirmPassword && <div className={styles.error}>{errors.confirmPassword.message}</div>}
+                    </div>
                     <div className={styles.btns}>
                         <button className={styles.btn}>Cancel</button>
-                        <button>Register</button>
+                        <button type="submit">Register</button>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     );
