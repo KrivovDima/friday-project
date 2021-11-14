94f226a97e3cec51ac1693831e036a3c062b723a
Eugen <krizhik@gmail.com>
2021-11-15 00:04:04 +0300

    login, logout thunks. error handling. not all types done

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import s from './Login.module.css'
-import {setIsLoggedIn} from '../../store/loginReducer';
+import {loginTC, logoutTC, setIsLoggedIn} from '../../store/loginReducer';
 import {AppRootStateType} from '../../store/store';
 
 export const Login = () => {
     const dispatch = useDispatch()
-    const isLoggedIn = useSelector((state:AppRootStateType)=> state.login.isLoggedIn)
+    const isLoggedIn = useSelector((state: AppRootStateType) => state.login.isLoggedIn)
+    const error = useSelector((state: AppRootStateType) => state.login.userData.error)
 
     const onLogin = () => {
-            dispatch(setIsLoggedIn({isLoggedIn: true}))
+        dispatch(loginTC({email: 'nya-admin@nya.nya', password: '1qazxcvBG', rememberMe: false}))
+    }
+    const onLogout = () => {
+        dispatch(logoutTC())
     }
 
     /*if (isLoggedIn) {
@@ -25,7 +29,6 @@ export const Login = () => {
                     <label className={s.description} htmlFor="email">Email</label>
                     <input type="text" id="email" name="username" placeholder={'enter your e-mail'}/>
                 </div>
-
                 <div>
                     <label className={s.description} htmlFor="pass">Password</label>
                     <input type="password" id="pass" name="password" placeholder={'enter your password'}/>
@@ -34,9 +37,9 @@ export const Login = () => {
                 <button className={s.mainButton} onClick={onLogin}>Login</button>
             </form>
             <span className={s.withoutAcc}>Don't have an account?</span>
-
             <a className={s.signUp}>Sign Up</a>
-
+            {/*<button className={s.mainButton} onClick={onLogout}>Logout</button>*/} {/*button for logout tests*/}
+            {error}
         </div>
     )
 }
--- src/store/loginReducer.ts
+++ src/store/loginReducer.ts
@@ -1,35 +1,85 @@
 import {Dispatch} from 'redux'
+import {loginApi, LoginParamsType} from '../api/login-api';
 
-const initialState: InitialStateType = {
-    isLoggedIn: false
+type UserDataType = {
+    _id: string
+    email: string
+    name: string
+    avatar?: string
+    publicCardPacksCount: number
+    created: Date | null
+    updated: Date | null
+    isAdmin: boolean
+    verified: boolean
+    rememberMe: boolean
+    error?: string
 }
 type InitialStateType = {
     isLoggedIn: boolean
+    userData: UserDataType
 }
 
-export const loginReducer = (state = initialState, action: ActionsType): any => {
+
+const initialState: InitialStateType = {
+    isLoggedIn: false,
+    userData: {
+        _id: '',
+        email: '',
+        name: '',
+        avatar: '',
+        publicCardPacksCount: 0,
+        created: null,
+        updated: null,
+        isAdmin: false,
+        verified: false,
+        rememberMe: false,
+        error: '',
+    }
+}
+
+export const loginReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
         case 'SET-IS-LOGGED-IN':
-        return {...state, isLoggedIn: action.payload.isLoggedIn}
+            return {...state, isLoggedIn: action.payload.isLoggedIn, userData: {...action.payload.userData}};
+        case 'SET-IS-LOGGED-OUT':
+            return {...state, isLoggedIn: action.payload.isLoggedIn, userData: {...initialState.userData}};
+        case 'SET-ERROR':
+            const copyState = {...state}
+            copyState.userData.error = action.payload.error
+            return copyState
         default:
             return state
     }
 }
 
 
-// actions
-export const setIsLoggedIn = (payload: { isLoggedIn: boolean }) => ({type: 'SET-IS-LOGGED-IN', payload} as const)
+export const setIsLoggedIn = (payload: { isLoggedIn: boolean, userData: UserDataType }) => ({
+    type: 'SET-IS-LOGGED-IN',
+    payload
+} as const)
+export const setIsLoggedOut = (payload: { isLoggedIn: boolean }) => ({type: 'SET-IS-LOGGED-OUT', payload} as const)
+export const setError = (payload: { isLoggedIn: boolean, error: string }) => ({type: 'SET-ERROR', payload} as const)
 
 
-// thunks
-export const firstLoginReducerTC = (payload: string) => (dispatch: Dispatch) => {
-    //servers requests
+export const loginTC = (data: LoginParamsType) => async (dispatch: Dispatch) => {
+    try {
+        const response = await loginApi.login(data)
+        dispatch(setIsLoggedIn({isLoggedIn: true, userData: {...response.data}}))
+    } catch (e: any) {
+        e.response.data.error ?
+            dispatch(setError({isLoggedIn: false, error: e.response.data.error})) :
+            dispatch(setError({isLoggedIn: false, error: 'some error occurred'}))
+    }
+}
+export const logoutTC = () => async (dispatch: Dispatch) => {
+    await loginApi.logout()
+    dispatch(setIsLoggedOut({isLoggedIn: false}))
 }
 
 
-// types
-
 type ActionsType =
     | ReturnType<typeof setIsLoggedIn>
+    | ReturnType<typeof setIsLoggedOut>
+    | ReturnType<typeof setError>
 
 type ThunkDispatch = any
